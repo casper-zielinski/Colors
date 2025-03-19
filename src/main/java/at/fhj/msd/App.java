292c8ff96a1e3ec6650a883b03b13f1d199f6ff7
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-03-19 23:12:39 +0100

    random

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,15 +1,27 @@
 package at.fhj.msd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Colors color = new Colors(255, 0, 0);
-        System.out.println(color.toString());
-        System.out.println(color.hexFormat());
-        System.out.println();
+        Colors red = new Colors(255, 0, 0);
+        Colors green = new Colors(0, 255, 0);
+        Colors blue = new Colors(0, 0, 255);
+        Colors red2 = new Colors(255, 0, 0);
+        List<Colors> colory = new ArrayList<>();
+        colory.add(red);
+        colory.add(green);
+        colory.add(blue);
+        colory.add(red2);
+        Colors.uniqColors(colory);
+
+       Colors.random(10);
+
+        
         
     }
 }
--- src/main/java/at/fhj/msd/Colors.java
+++ src/main/java/at/fhj/msd/Colors.java
@@ -1,5 +1,10 @@
 package at.fhj.msd;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.attribute.standard.ColorSupported;
+
 public class Colors {
       private int red;
       private int green;
@@ -15,14 +20,63 @@ public class Colors {
                   throw new IllegalArgumentException("Only values from 0 to 255 are available");
             } 
       }
+
       public String hexFormat()
       {
-            return String.format("#%s%s%s", Integer.toHexString(red).toUpperCase(),
+            return String.format("\"#%s%s%s\"", Integer.toHexString(red).toUpperCase(),
             Integer.toHexString(green).toUpperCase(), Integer.toHexString(blue).toUpperCase());
       }
+
       public String rgbFormat()
       {
-            return null;
+            return String.format("\"rgb(%d, %d, %d)\"", this.red, this.green, this.blue);
       }
+
+      @Override
+      public String toString()
+      {
+            return String.format("\'rgb(%d, %d, %d)\'", this.red, this.green, this.blue);
+      }
+
+      public boolean equals(Colors other)
+      {
+            return (other.red == this.red) && (other.green == this.green)
+            && (other.blue == this.blue);
+      }
+
+      public static List<Colors> random(int amount)
+      {
+            List<Colors> colors = new ArrayList<>();
+            for (int i = 0; i < amount; i++)
+            {
+                  int r = (int) (Math.random() * 255);
+                  int g = (int) (Math.random() * 255);
+                  int b = (int) (Math.random() * 255);
+                  Colors color = new Colors(r, g, b);
+                  colors.add(color);
+            }
+            return colors;
+      }
+
+      public static List<Colors> uniqColors(List<Colors> colors)
+      {
+            List<Colors> colours = new ArrayList<>();
+            for (int i = 0; i < colors.size(); i++)
+            {
+                  for (int j = 0; j < colors.size(); j++)
+                  {
+                        if ((colors.get(i)).equals(colors.get(j)))
+                        {
+                              break;
+                        }
+                        else
+                        {
+                              colours.add(colors.get(i));
+                        }
+                  }
+            }
+            return colours;
+      }
+
 
 }
